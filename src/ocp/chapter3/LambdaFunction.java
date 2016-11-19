67ba929d4f7754e1755440f3f9c2254a9c9de2a2
AJ Catambay <adrianjayson13@gmail.com>
2016-11-19 20:50:52 +0800

    studied lambdas

---
+++ src/ocp/chapter3/LambdaFunction.java
@@ -0,0 +1,9 @@
+package ocp.chapter3;
+
+/**
+ * Created by adrianjayson on 11/19/16.
+ */
+@FunctionalInterface
+public interface LambdaFunction {
+    void talk();
+}
--- src/ocp/chapter3/TestClass.java
+++ src/ocp/chapter3/TestClass.java
@@ -11,13 +11,26 @@ public class TestClass {
     }
 
     public static void main(String[] args) {
-        TestClass tc = new TestClass(Grade.A);
-        System.out.println("Your grade is " + tc.g.getGrade() + "."); // 90
-        System.out.println("Your grade is " + tc.g.name() + "."); // A
-        System.out.println("Your grade is " + tc.g.valueOf("B") + "."); // B
-
+//        TestClass tc = new TestClass(Grade.A);
+//        System.out.println("Your grade is " + tc.g.getGrade() + "."); // 90
+//        System.out.println("Your grade is " + tc.g.name() + "."); // A
+//        System.out.println("Your grade is " + tc.g.valueOf("B") + "."); // B
+//
         for (Grade gr : Grade.values()) {
-            System.out.println("Grade of " + gr.name() + " means " + gr.getGrade());
+            System.out.println("Grade of " + gr + " means " + gr.getGrade());
         }
+
+        LambdaFunction lf = () -> System.out.println("I love you Rachel!");
+        lf.talk();
+
+        BlockLambda bl = (i) -> {
+            if(i%2 == 0) {
+                return "Even";
+            }
+
+            return "Odd";
+        };
+
+        System.out.println(bl.checkNumber(5));
     }
 }
